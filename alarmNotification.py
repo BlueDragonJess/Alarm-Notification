bc154884376e31d7a683bc6c97830b4333842246
Jessica Camacho <151972947+BlueDragonJess@users.noreply.github.com>
2024-03-25 23:33:44 +0100

    Create alarmNotification.py

---
+++ alarmNotification.py
@@ -0,0 +1,12 @@
+current_hours = float(input("""What is your current time/hours (24 hour clock)?
+Note: Give the hours as a single number with decimals (ex: 23,30 for 23:30)"""))
+alarm_hours = float(input("How many hours do you wish to wait until the alarm goes off?: "))
+
+
+def findHours(time, alarm):
+    days = alarm_hours // 24
+    hours = (current_hours + alarm_hours) % 24
+    return "The alarm will go off in {} days, at {}.".format(int(days), round(hours, 2))
+
+
+print(findHours(current_hours, alarm_hours))
