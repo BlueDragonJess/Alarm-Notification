6306b2239f3691467924f286d5ca15f97e591172
Jessica Camacho <151972947+BlueDragonJess@users.noreply.github.com>
2024-03-25 23:38:32 +0100

    Update alarmNotification.py

--- alarmNotification.py
+++ alarmNotification.py
@@ -1,5 +1,5 @@
 current_hours = float(input("""What is your current time/hours (24 hour clock)?
-Note: Give the hours as a single number with decimals (ex: 23,30 for 23:30)"""))
+Note: Give the hours as a single number with decimals (ex: 23.30 for 23:30)"""))
 alarm_hours = float(input("How many hours do you wish to wait until the alarm goes off?: "))
 
 
